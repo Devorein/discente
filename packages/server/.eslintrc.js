50929d83fbdad927362ba1cc987e996af6c72d97
Safwan Shaheer <devorein00@gmail.com>
2022-06-26 17:12:37 +0600

    Added config files for server package

---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const eslintConfig = require('../../.eslintrc');
+
+eslintConfig.parserOptions.project = [path.join(__dirname, './tsconfig.eslintrc.json')];
+eslintConfig.rules["no-undef"] = "off";
+eslintConfig.rules["no-unused-vars"] = "off";
+eslintConfig.rules["no-redeclare"] = "off";
+eslintConfig.rules["no-use-before-define"] = "off";
+eslintConfig.rules["jest/expect-expect"] = [
+  "error",
+  {
+    "assertFunctionNames": ["expect", "expect*", "assertSupertestErrorRequest", "assertSupertestSuccessRequest"]
+  }
+]
+
+eslintConfig.overrides = [
+  {
+    "files": ["tests/**"],
+    "plugins": ["jest"],
+    "extends": ["plugin:jest/recommended"],
+    rules: {
+      "jest/no-focused-tests": "off"
+    }
+  }
+]
+
+
+module.exports = eslintConfig;
