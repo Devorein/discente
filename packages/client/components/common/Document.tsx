56e8b2e50840ac4dbd7170f512be73f9dd2bf7ed
Safwan Shaheer <devorein00@gmail.com>
2022-06-27 17:32:06 +0600

    Added Document component with Head and Providers to wrap Page components

---
+++ packages/client/components/common/Document.tsx
@@ -0,0 +1,37 @@
+import { Paper } from '@mui/material';
+import Head from 'next/head';
+import { ReactNode } from 'react';
+import { QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import Providers from './Providers';
+
+export default function Document({
+  children,
+  queryClient = new QueryClient(),
+  showReactQueryDevTools = true
+}: {
+  showReactQueryDevTools?: boolean;
+  children: ReactNode;
+  queryClient?: QueryClient;
+}) {
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='/discente.ico' />
+        <title>Discente</title>
+      </Head>
+      <Providers queryClient={queryClient}>
+        <Paper
+          elevation={0}
+          sx={{
+            borderRadius: 0,
+            p: 2
+          }}
+        >
+          {children}
+          {showReactQueryDevTools && <ReactQueryDevtools />}
+        </Paper>
+      </Providers>
+    </>
+  );
+}
--- packages/client/components/common/index.ts
+++ packages/client/components/common/index.ts
@@ -1,8 +1,12 @@
+import Document from './Document';
 import FormLabelWithHelper from './FormLabelWithHelper';
 import PasswordInput from './PasswordInput';
+import Providers from './Providers';
 import TextInput from './TextInput';
 
 export {
+  Document,
+  Providers,
   PasswordInput,
   TextInput,
   FormLabelWithHelper
