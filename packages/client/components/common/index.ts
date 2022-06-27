0697e512f8e0bdd5a0e4fbc3947c6a90edaede74
Safwan Shaheer <devorein00@gmail.com>
2022-06-27 17:22:09 +0600

    Shared component to show and hide password value field

---
+++ packages/client/components/common/index.ts
@@ -0,0 +1,9 @@
+import FormLabelWithHelper from './FormLabelWithHelper';
+import PasswordInput from './PasswordInput';
+import TextInput from './TextInput';
+
+export {
+  PasswordInput,
+  TextInput,
+  FormLabelWithHelper
+};
