3bab6567dca3f7cd0f40305d1e98e33d5abaacfc
Safwan Shaheer <devorein00@gmail.com>
2022-07-10 17:24:13 +0600

    Added tests for authController.login function

--- packages/server/tests/controllers/authController.test.ts
+++ packages/server/tests/controllers/authController.test.ts
@@ -1,9 +1,11 @@
 import { User } from '@prisma/client';
 import { v4 } from 'uuid';
-import { UniqueConstraintViolationError } from '../../src/ApiError';
+import { UniqueConstraintViolationError, UserNotFoundError } from '../../src/ApiError';
+import { prisma } from '../../src/config';
 import authController from '../../src/controllers/authController';
 import { registerUser } from '../../src/models/User';
-import { RegisterUserPayload, SuccessApiResponse, UserWithoutSecretFields } from '../../src/types';
+import { LoginUserPayload, RegisterUserPayload, SuccessApiResponse, UserWithoutSecretFields } from '../../src/types';
+import { hashPassword } from '../../src/utils';
 import { mockRequest, mockResponse } from '../helpers/mocks';
 import { expectUserResponse } from '../helpers/user';
 
@@ -12,7 +14,22 @@ let activeUser: UserWithoutSecretFields;
 
 const userPassword = 'Secret123$$';
 
+// test this user for login, registration
+let privateUser: User;
+
 beforeAll(async () => {
+  const userId = v4();
+  privateUser = (await prisma.user.create({
+    data: {
+      id: userId,
+      email: `${v4()}@gmail.com`,
+      hashedPass: await hashPassword(userPassword),
+      username: v4(),
+      status: 'private',
+      name: "John Doe Private"
+    }
+  }));
+
   activeUser = await registerUser({
     email: `${v4()}@gmail.com`,
     password: userPassword,
@@ -65,6 +82,54 @@ describe('authController.register', () => {
     );
     expect(mockedResponse.status).toHaveBeenCalledWith(200);
     expectUserResponse(registerUserPayload, mockedResponseData.data);
+    expect(mockedResponseData.data).not.toHaveProperty('password');
+  });
+});
+
+describe('authController.login', () => {
+  it(`Should fail on incorrect username or email`, async () => {
+    const mockedRequest = mockRequest<LoginUserPayload>({
+      body: {
+        usernameOrEmail: `x${privateUser.email}`,
+        password: userPassword
+      }
+    });
+    const mockedResponse = mockResponse();
+    await authController.login(mockedRequest, mockedResponse);
+
+    expect(mockedResponse.status).toHaveBeenCalledWith(
+      UserNotFoundError.statusCode
+    );
+    expect(mockedResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      error: UserNotFoundError.message
+    });
+  });
+
+  it(`Should pass and set cookies correctly if valid email or username and password is provided`, async () => {
+    const loginUserPayload: LoginUserPayload = {
+      usernameOrEmail: privateUser.email,
+      password: userPassword,
+      remember: true
+    };
+    const mockedRequest = mockRequest<LoginUserPayload>({
+      body: loginUserPayload
+    });
+    const mockedResponse = mockResponse();
+
+    await authController.login(mockedRequest, mockedResponse);
+
+    const mockedResponseData = mockedResponse.mockedJson.mock
+      .calls[0][0] as SuccessApiResponse<UserWithoutSecretFields>;
+    expect(mockedResponse.mockedCookie.mock.calls[0][1].length).toBeGreaterThan(
+      0
+    );
+    expect(mockedResponse.mockedCookie.mock.calls[0][2].expires).toBeTruthy();
+    expect(mockedResponse.status).toHaveBeenCalledWith(200);
+    expectUserResponse(
+      privateUser,
+      mockedResponseData.data
+    );
     expect(mockedResponseData.data).not.toHaveProperty('password');
   });
 });
