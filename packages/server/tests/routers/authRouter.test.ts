5a330d85ff6a7dd2b9afc83c642d6275741878af
Safwan Shaheer <devorein00@gmail.com>
2022-07-10 18:11:19 +0600

    Added tests for /auth/login route

--- packages/server/tests/routers/authRouter.test.ts
+++ packages/server/tests/routers/authRouter.test.ts
@@ -1,6 +1,9 @@
+import { User } from "@prisma/client";
 import { v4 } from "uuid";
+import { registerUser } from "../../src/models";
 import authRouter from "../../src/routers/authRouter";
 import {
+  LoginUserPayload,
   RegisterUserPayload
 } from '../../src/types';
 import { hashPassword } from "../../src/utils";
@@ -10,7 +13,17 @@ const { assertSupertestErrorRequest, assertSupertestSuccessRequest, router } = c
 
 router.use('/auth', authRouter);
 
+let user: User;
 const userPassword = 'Secret123$';
+
+beforeAll(async () => {
+  user = await registerUser({
+    email: `${v4()}@gmail.com`,
+    password: userPassword,
+    username: v4().slice(0, 10),
+    name: 'John Doe'
+  })
+})
 
 describe('POST /auth/register', () => {
   it(`Should fail if incorrect payload is passed`, async () => {
@@ -33,6 +46,30 @@ describe('POST /auth/register', () => {
         password: await hashPassword(userPassword),
         username: v4().slice(0, 10),
         name: 'John Doe'
+      }
+    })
+  })
+})
+
+describe('POST /auth/login', () => {
+  it(`Should fail if incorrect payload is passed`, async () => {
+    await assertSupertestErrorRequest({
+      endpoint: 'auth/login',
+      method: "post",
+      statusCode: 400,
+      payload: {
+        name: "Hello World"
+      }
+    })
+  })
+
+  it(`Should pass if correct payload is passed`, async () => {
+    await assertSupertestSuccessRequest<LoginUserPayload>({
+      endpoint: 'auth/login',
+      method: "post",
+      payload: {
+        usernameOrEmail: user.username,
+        password: userPassword
       }
     })
   })
