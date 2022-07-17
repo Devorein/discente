4abddf46eebdd7e9bd1b704f68be9921d8278238
Safwan Shaheer <devorein00@gmail.com>
2022-07-17 16:26:57 +0600

    Updated components to consume updated api hooks

--- packages/client/api/queries/useGetCurrentUserQuery.ts
+++ packages/client/api/queries/useGetCurrentUserQuery.ts
@@ -1,20 +1,15 @@
 import { apiConstants } from '@constants';
-import { ApiResponse, GetCurrentUserResponse } from '@types';
+import { GetCurrentUser } from '@types';
 import { useApiQuery, useQueryClientSetData } from 'hooks';
 
 export function useGetCurrentUserQueryData() {
-  const queryClientSetData = useQueryClientSetData<GetCurrentUserResponse>();
-  return (
-    cacheHitCb: (
-      queryResponse: ApiResponse<GetCurrentUserResponse> | null | undefined
-    ) => ApiResponse<GetCurrentUserResponse> | null | undefined
-  ) => {
-    queryClientSetData(apiConstants.getCurrentUser.key(), cacheHitCb);
-  };
+  return useQueryClientSetData<GetCurrentUser>(
+    apiConstants.getCurrentUser.key()
+  );
 }
 
 export function useGetCurrentUserQuery() {
-  return useApiQuery<GetCurrentUserResponse>(
+  return useApiQuery<GetCurrentUser>(
     apiConstants.getCurrentUser.key(),
     apiConstants.getCurrentUser.endpoint
   );
