a54372399c76d5a922d73a357c2d24ecd817043f
Safwan Shaheer <devorein00@gmail.com>
2022-07-18 18:43:58 +0600

    Added controller method to update user

--- packages/server/src/controllers/userController.ts
+++ packages/server/src/controllers/userController.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express';
-import {
-  deleteUserById
-} from '../models/User';
-import {
-  DeleteUser, User
-} from '../types';
+import { deleteUserById, updateUserById } from '../models/User';
+import { DeleteUser, UpdateUser, User } from '../types';
 import {
   handleError,
-  handleSuccess, removeCookieFromResponse
+  handleSuccess,
+  removeCookieFromResponse,
+  removeSecretUserFields
 } from '../utils';
 
 const userController = {
+  update: async (
+    req: Request<any, any, UpdateUser['payload']>,
+    res: Response<UpdateUser['response']>
+  ) => {
+    try {
+      const user = req.user as User;
+      const updateData = req.body;
+      const resUser = await updateUserById(user.id, updateData);
+      return handleSuccess<UpdateUser['data']>(
+        res,
+        removeSecretUserFields(resUser)
+      );
+    } catch (err) {
+      return handleError(res, err);
+    }
+  },
+
   delete: async (req: Request, res: Response) => {
     try {
       const user = req.user as User;
